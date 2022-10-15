247f0b60107acef4dc732b5a6b61f9f86dcf24b0
Chinweikpe Ejuh <chinwesen@gmail.com>
2022-10-14 21:26:26 -0700

    I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point 
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
+
