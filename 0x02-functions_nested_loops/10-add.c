fb0b88c09ce54300b6d9d12ff11d4f9dd45297ce
Chinweikpe Ejuh <chinwesen@gmail.com>
2022-10-19 21:22:19 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - prints the sum between two values
+ * @a: value 1
+ * @b: value 2
+ * Return: success 1
+ * on error, -1 is returned
+ */
+int add(int a, int b)
+{
+	int r;
+
+	r = (a + b);
+	return (r);
+}
